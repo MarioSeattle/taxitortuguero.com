976a48bb9e2c47d0c3b124e465ab07ad86d5e6f7
MarioSeattle <marioseattle@live.com>
2016-09-26 11:01:27 -0700

    Testing and ml files

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://icono-49d6.kxcdn.com/icono.min.css">
+    <meta charset="UTF-8">
+    <title>Tortuguero WaterTaxi</title>
+</head>
+<body>
+<!--Logo Banner-->
+<header role="banner">
+    <a href="http://tortuguerotaxi.com/"><img id="logo-main" src="images/logo.png" width="400" alt="Logo Principal"></a>
+</header><!-- header role="banner" -->
+
